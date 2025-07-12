e65dc742a3d0a3e93ea69636f971bdb354597b41
AMAL <amalkochu000@gmail.com>
2025-07-12 22:29:57 +0530

    student registration and login setup done

--- server/app/models/model1.py
+++ server/app/models/model1.py
@@ -2,14 +2,39 @@ from app.extensions import db
 
 class Login(db.Model):
     __tablename__ = 'login_info' 
-    id = db.Column(db.Integer, primary_key=True)
+    logid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    user_type = db.Column(db.Enum('student', 'warden', 'admin'),nullable=False,default='student')
+
+    student_info = db.relationship('Students', back_populates='login', uselist=False)
+
+class Students(db.Model):
+    __tablename__ = 'student_info'
+    student_id = db.Column(db.Integer,primary_key=True)
+    full_name = db.Column(db.String(150),nullable=False)
+    email = db.Column(db.String(150),nullable=False)
+    phone_number = db.Column(db.String(150),nullable=False)
+    date_of_birth = db.Column(db.Date,nullable=False)
+    address = db.Column(db.Text,nullable=False)
+    admission_date = db.Column(db.Date,nullable=False)
+    room_number = db.Column(db.String(150),nullable=False,default='pending')
+    emergency_contact_name = db.Column(db.String(150),nullable=False)
+    emergency_contact_phone = db.Column(db.String(150),nullable=False)
+    guardian_name = db.Column(db.String(150),nullable=False)
+    photo_url = db.Column(db.String(150),nullable=False)
+    password_hash = db.Column(db.String(150),nullable=False)
+    status = db.Column(db.Enum('inactive','active','pending'),nullable=False,default='pending')
+    user_type = db.Column(db.String(150),nullable=False,default='student')
+    logid = db.Column(db.Integer,db.ForeignKey('login_info.logid'),nullable=False)
+
+    login = db.relationship('Login', back_populates='student_info')
+
+class Warden(db.Model):
+    __tablename__ = 'warden_info'
+    
 
 
-# class Students(db.Model):
-#     __tablenaem__ = 'student_info'
-#     id = db.Column(db.Integer,primary_key=True)
-#     user_type = db.column(db.String(150),nullable=False,default='student')
+
 
     
--- server/app/routes/auth.py
+++ server/app/routes/auth.py
@@ -1,29 +1,93 @@
 from flask import *
 from flask import render_template
-from app.models.model1 import Login
+from app.models.model1 import *
 from app.extensions import db
 auth_bp = Blueprint('auth',__name__)
 
+
+#? ------------login route----------------
+
 @auth_bp.route('/',methods=['POST','GET'])
+def main():
+    return render_template('login.html')
+
+#? ------------login function----------------
+
+@auth_bp.route('/login',methods=['POST'])
 def login():
-    if request.method == 'POST':
-        username=request.form['username']
-        password=request.form['password']
+    try:
+        if request.method == 'POST':
+            username=request.form['username']
+            password=request.form['password']
 
-        new_user = Login(username=username,password=password)
-        db.session.add(new_user)
-        db.session.commit()
+            #? get the entered username and password from the table
+            user = db.session.query(Login).filter_by(username=username).first()
+            passkey = db.session.query(Login).filter_by(password=password).first()
 
-        return 'user added to login table'
-    
-    return render_template('login.html')    
 
-@auth_bp.route('/register',methods=['post','get'])
+            #? check if username and password is correct
+            if user and passkey :
+                #? check if the user is student 
+                if  not user.user_type =='student':
+                    return 'Student profile not found',403
+                #? check if student status is acitve or not  
+                if user.student_info.status != 'active':
+                    return 'Login blocked , status {user.student_info.status}',403
+                else:
+                    return 'login successful',200
+            else :
+                return 'invalid username or password',401
+    except ValueError as ve:
+        return{'error:str(ve)'},401
+
+#? ------------student registration route----------------
+
+@auth_bp.route('/registerLink',methods=['POST','GET'])
 def registerLink():
     return render_template('student-reg-form.html')
 
-# @auth_bp.route('/students_registration',methods=['post'])
-# def registrationStudents():
-#     if request.method=='POST':
-        
+#? ------------student registration function----------------
+
+
+@auth_bp.route('/registrationStudents',methods=['POST'])
+def registrationStudents():
+    try:
+        if request.method=='POST':
+            fullname = request.form['full_name']
+            email = request.form['email']
+            phone = request.form['phone_number']
+            dob = request.form['date_of_birth']
+            address = request.form['address']
+            admission_date = request.form['admission_date']
+            em_contact_name = request.form['emergency_contact_name']
+            em_contact_number = request.form['emergency_contact_phone']
+            guardian_name = request.form['guardian_name']
+            url = request.form['photo_url']
+            password = request.form['password']
+
+            new_user = Students(full_name=fullname,email=email,phone_number=phone,date_of_birth=dob,address=address,admission_date=admission_date,emergency_contact_name=em_contact_name,emergency_contact_phone=em_contact_number,guardian_name=guardian_name,photo_url=url,password_hash=password)
+            
+            new_user_login = Login(username=email,password=password)
+
+            db.session.add(new_user_login)
+            db.session.commit()
+
+            new_user = Students(full_name=fullname,email=email,phone_number=phone,date_of_birth=dob,address=address,admission_date=admission_date,emergency_contact_name=em_contact_name,emergency_contact_phone=em_contact_number,guardian_name=guardian_name,photo_url=url,password_hash=password,logid=new_user_login.logid)
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return 'user added to login table'
+
+    except ValueError as ve:
+        return {'error:str(ve)'},401
+    
+
+
+#? ------------warden home route----------------
+
+@auth_bp.route('/wardenDashboard',methods=['post','get'])
+def wardenDashboard():
+    return render_template('')
+
 
